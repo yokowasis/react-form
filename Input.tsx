999b3f9a3b57dee112f81fae735485092a462ce4
yokowasis@nitro5 <yokowasis@gmail.com>
2023-08-25 15:29:22 +0800

    Autocomplete OK

--- Input.tsx
+++ Input.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { icons } from "./Types";
+import "./Input.scss";
 
 type AppProps = {
   type: "text" | "select";
@@ -8,17 +10,15 @@ type AppProps = {
   placeholder?: string;
   iconAfter?: icons;
   iconBefore?: icons;
-  data?:
-    | {
-        label: string;
-        value: string;
-      }[]
-    | string[];
+  data?: string[];
 };
 
 export default function Input(props: AppProps) {
+  const [filteredData, setFilteredData] = useState<string[]>([]);
+  const [value, setValue] = useState("");
+
   return props.type === "text" ? (
-    <>
+    <div style={{ position: "relative" }} class={"mb-3"}>
       {props.label ? (
         <div className={props.labelClass}>
           <label for={`${props.id}`}>{props.label}</label>
@@ -26,7 +26,7 @@ export default function Input(props: AppProps) {
       ) : (
         <></>
       )}
-      <div class="input-group mb-3">
+      <div class="input-group">
         {props.iconBefore ? (
           <>
             <div class="input-group-prepend">
@@ -44,6 +44,21 @@ export default function Input(props: AppProps) {
           class="form-control"
           placeholder={props.placeholder}
           id={props.id}
+          value={value}
+          onChange={(e) => {
+            const target = e.target as any as { value: string };
+            const val = target.value as string;
+            setValue(val);
+            if (val === "") {
+              setFilteredData([]);
+            } else {
+              setFilteredData(
+                props.data?.filter((item) =>
+                  item.toLowerCase().includes(target.value.toLowerCase())
+                ) as string[]
+              );
+            }
+          }}
         />
         {props.iconAfter ? (
           <>
@@ -57,18 +72,36 @@ export default function Input(props: AppProps) {
           <></>
         )}
       </div>
-    </>
+      {filteredData.length > 0 ? (
+        <>
+          <div
+            className={"bg-light w-100 border"}
+            style={{ position: "absolute" }}
+          >
+            {filteredData.map((item) => (
+              <div
+                onClick={() => {
+                  setValue(item);
+                  setFilteredData([]);
+                }}
+                className={"autoInputChildren p-2"}
+              >
+                {item}
+              </div>
+            ))}
+          </div>
+        </>
+      ) : (
+        <></>
+      )}
+    </div>
   ) : props.type === "select" ? (
     <div class="form-group">
       <label for="exampleFormControlSelect1">Example select</label>
       <select class="form-control" id={props.id}>
-        {props.data?.map((item) =>
-          typeof item === "string" ? (
-            <option>{item}</option>
-          ) : (
-            <option value={item.value}>{item.label}</option>
-          )
-        )}
+        {props.data?.map((item) => (
+          <option>{item}</option>
+        ))}
       </select>
     </div>
   ) : (
