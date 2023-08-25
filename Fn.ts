79b3f4e118e18902f945455a7b02dedc8b416b44
yokowasis@nitro5 <yokowasis@gmail.com>
2023-08-25 20:45:01 +0800

    parsejwt

--- Fn.ts
+++ Fn.ts
@@ -88,6 +88,22 @@ export function getStorageVar(key: string) {
   return JSON.parse(s);
 }
 
+export function parseJwt(token: string) {
+  var base64Url = token.split(".")[1];
+  var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  var jsonPayload = decodeURIComponent(
+    window
+      .atob(base64)
+      .split("")
+      .map(function (c) {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join("")
+  );
+
+  return JSON.parse(jsonPayload);
+}
+
 export async function imgURLtoBase64(url: string): Promise<string> {
   const res = await fetch(url);
   const blob = await res.blob();
