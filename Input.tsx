f18532ab09183638b11ee6006f10cf71cb47b1bc
yokowasis@nitro5 <yokowasis@gmail.com>
2023-08-25 15:01:53 +0800

    label class

--- Input.tsx
+++ Input.tsx
@@ -3,6 +3,8 @@ import { icons } from "./Types";
 type AppProps = {
   type: "text" | "select";
   id: string;
+  label?: string;
+  labelClass?: string;
   placeholder?: string;
   iconAfter?: icons;
   iconBefore?: icons;
@@ -16,37 +18,46 @@ type AppProps = {
 
 export default function Input(props: AppProps) {
   return props.type === "text" ? (
-    <div class="input-group mb-3">
-      {props.iconBefore ? (
-        <>
-          <div class="input-group-prepend">
-            <span class="input-group-text">
-              <i className={`bi-${props.iconBefore}`}></i>
-            </span>
-          </div>
-        </>
+    <>
+      {props.label ? (
+        <div className={props.labelClass}>
+          <label for={`${props.id}`}>{props.label}</label>
+        </div>
       ) : (
         <></>
       )}
+      <div class="input-group mb-3">
+        {props.iconBefore ? (
+          <>
+            <div class="input-group-prepend">
+              <span class="input-group-text">
+                <i className={`bi-${props.iconBefore}`}></i>
+              </span>
+            </div>
+          </>
+        ) : (
+          <></>
+        )}
 
-      <input
-        type="text"
-        class="form-control"
-        placeholder={props.placeholder}
-        id={props.id}
-      />
-      {props.iconAfter ? (
-        <>
-          <div class="input-group-append">
-            <span class="input-group-text">
-              <i className={`bi-${props.iconAfter}`}></i>
-            </span>
-          </div>
-        </>
-      ) : (
-        <></>
-      )}
-    </div>
+        <input
+          type="text"
+          class="form-control"
+          placeholder={props.placeholder}
+          id={props.id}
+        />
+        {props.iconAfter ? (
+          <>
+            <div class="input-group-append">
+              <span class="input-group-text">
+                <i className={`bi-${props.iconAfter}`}></i>
+              </span>
+            </div>
+          </>
+        ) : (
+          <></>
+        )}
+      </div>
+    </>
   ) : props.type === "select" ? (
     <div class="form-group">
       <label for="exampleFormControlSelect1">Example select</label>
