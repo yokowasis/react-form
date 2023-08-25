dfc589125894ee384766cbbbdc5d38a278ae8c0f
yokowasis@nitro5 <yokowasis@gmail.com>
2023-08-25 14:56:47 +0800

    types and icon

--- Input.tsx
+++ Input.tsx
@@ -1,3 +1,66 @@
-export default function Input() {
-  return <input type="text" />;
+import { icons } from "./Types";
+
+type AppProps = {
+  type: "text" | "select";
+  id: string;
+  placeholder?: string;
+  iconAfter?: icons;
+  iconBefore?: icons;
+  data?:
+    | {
+        label: string;
+        value: string;
+      }[]
+    | string[];
+};
+
+export default function Input(props: AppProps) {
+  return props.type === "text" ? (
+    <div class="input-group mb-3">
+      {props.iconBefore ? (
+        <>
+          <div class="input-group-prepend">
+            <span class="input-group-text">
+              <i className={`bi-${props.iconBefore}`}></i>
+            </span>
+          </div>
+        </>
+      ) : (
+        <></>
+      )}
+
+      <input
+        type="text"
+        class="form-control"
+        placeholder={props.placeholder}
+        id={props.id}
+      />
+      {props.iconAfter ? (
+        <>
+          <div class="input-group-append">
+            <span class="input-group-text">
+              <i className={`bi-${props.iconAfter}`}></i>
+            </span>
+          </div>
+        </>
+      ) : (
+        <></>
+      )}
+    </div>
+  ) : props.type === "select" ? (
+    <div class="form-group">
+      <label for="exampleFormControlSelect1">Example select</label>
+      <select class="form-control" id={props.id}>
+        {props.data?.map((item) =>
+          typeof item === "string" ? (
+            <option>{item}</option>
+          ) : (
+            <option value={item.value}>{item.label}</option>
+          )
+        )}
+      </select>
+    </div>
+  ) : (
+    <></>
+  );
 }
